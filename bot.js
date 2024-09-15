eef66def2162e16d4d1ae2792104248c9f7f2d74
Yurii Danylenko <yurii_danylenko@epam.com>
2024-09-15 19:15:58 +0300

    Replaces require with import

--- bot.js
+++ bot.js
@@ -1,25 +1,18 @@
-require("dotenv").config();
-const express = require("express");
+import { config } from "dotenv";
 
-const {
-  Bot,
-  GrammyError,
-  HttpError,
-  Keyboard,
-  InlineKeyboard,
-  webhookCallback,
-} = require("grammy");
-const { Menu } = require("@grammyjs/menu");
+import { Bot, GrammyError, HttpError, Keyboard, InlineKeyboard } from "grammy";
+import { Menu } from "@grammyjs/menu";
 
+config();
 const bot = new Bot(process.env.API_TOKEN);
 
 bot.command("start", async (ctx) => {
   const keyboard = new Keyboard().text("Тест на рівень").text("Обрати книгу");
   await ctx.reply(
-    "*Greetings everyone\\!*\n\nBook Book \\- це читацький клуб рівнів A2 \\- B1\\.\n\nТут на вас чекають книги у \\.pdf форматі\\, аудіо та завдання до тексту\\.\n\n[Клікай](https://t\\.me/+9PYtuOuUufFjNzA0) щоб доєднатися до чату [Book Book readers](https://t\\.me/+9PYtuOuUufFjNzA0)\\.",
-    {
-      parse_mode: "MarkdownV2",
-    },
+      "*Greetings everyone\\!*\n\nBook Book \\- це читацький клуб рівнів A2 \\- B1\\.\n\nТут на вас чекають книги у \\.pdf форматі\\, аудіо та завдання до тексту\\.\n\n[Клікай](https://t\\.me/+9PYtuOuUufFjNzA0) щоб доєднатися до чату [Book Book readers](https://t\\.me/+9PYtuOuUufFjNzA0)\\.",
+      {
+        parse_mode: "MarkdownV2",
+      },
   );
   await ctx.reply("Чого душа бажає?", {
     reply_markup: keyboard,
@@ -28,8 +21,8 @@ bot.command("start", async (ctx) => {
 
 bot.hears("Тест на рівень", async (ctx) => {
   const inlineKeyboard = new InlineKeyboard().url(
-    "Тест тутоньки",
-    "https://www.cambridgeenglish.org/test-your-english/general-english",
+      "Тест тутоньки",
+      "https://www.cambridgeenglish.org/test-your-english/general-english",
   );
   await ctx.reply("3 хвилини і ти дізнаєшся свій рівень.\nGood luck!", {
     reply_markup: inlineKeyboard,
@@ -38,9 +31,9 @@ bot.hears("Тест на рівень", async (ctx) => {
 
 bot.hears("Обрати книгу", async (ctx) => {
   const booksKeyboard = new InlineKeyboard()
-    .url("The Phantom of the Opera", "https://t.me/c/2096048963/14")
-    .url("Frankenstein", "https://t.me/c/2096048963/81")
-    .url("Sherlock Holmes: The Emerald Crown", "https://t.me/c/2096048963/127");
+      .url("The Phantom of the Opera", "https://t.me/c/2096048963/14")
+      .url("Frankenstein", "https://t.me/c/2096048963/81")
+      .url("Sherlock Holmes: The Emerald Crown", "https://t.me/c/2096048963/127");
 
   await ctx.reply("Яку книгу будеш читати?", {
     reply_markup: booksKeyboard,
@@ -48,8 +41,8 @@ bot.hears("Обрати книгу", async (ctx) => {
 });
 
 const menu = new Menu("main-menu")
-  .text("Тест на рівень")
-  .text("Обрати книгу", (ctx) => ctx.reply("Обрати книгу"));
+    .text("Тест на рівень")
+    .text("Обрати книгу", (ctx) => ctx.reply("Обрати книгу"));
 
 bot.use(menu);
 
@@ -61,8 +54,8 @@ bot.command("start", async (ctx) => {
 
 bot.catch((e) => {
   console.error(
-    e,
-    `Error while handling the update ${e.ctx.update.update_id}:`,
+      e,
+      `Error while handling the update ${e.ctx.update.update_id}:`,
   );
 
   if (e.error instanceof GrammyError) {
@@ -82,13 +75,6 @@ bot.catch((e) => {
 process.once("SIGINT", () => bot.stop());
 process.once("SIGTERM", () => bot.stop()); // Stopping the bot when the Node.js process
 
-//
-// bot.start();
 
-// const app = express(); // or whatever you're using
-// app.use(express.json()); // parse the JSON request body
-
-// "express" is also used as default if no argument is given.
-// app.use(webhookCallback(bot, "express"));
 
 export default bot;
